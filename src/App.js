f7908d3d214d704b36d254ad331e777e0e21acbb
Erhart Fabian Castillo <A01274089@itesm.mx>
2022-02-27 17:33:37 -0600

    Routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter,Routes,Route, Router, Switch} from "react-router-dom";
+import {Home} from "./Components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+
+        <Route path="/home">
+          <Home/>
+        </Route>
+
+        <Route path="/" exact component={Home}></Route>
+
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/Components/Home/Home.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Home.css';
+
+
+function Home(props) {
+
+    return (
+        <div className="home">
+            <h2>Panteon Publico</h2>
+        </div>
+    )
+}
+
+export default Home;

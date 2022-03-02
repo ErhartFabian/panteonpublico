9627f2417e6f2ff68500439a297b3f658727bcae
Erhart Fabian Castillo <A01274089@itesm.mx>
2022-03-01 20:52:24 -0600

    Component FormaContacto

---
+++ src/Components/FormaContacto/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './FormaContacto.css';
+
+
+function FormaContacto() {
+
+    return (
+        <div className="home">
+            <h2>FormaContacto</h2>
+        </div>
+    )
+}
+
+export default FormaContacto;

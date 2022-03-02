32f336999c13fd6645f743c1390d04ad3901d606
Erhart Fabian Castillo <A01274089@itesm.mx>
2022-03-01 21:47:34 -0600

    forma de contacto sin css

--- src/Components/FormaContacto/index.js
+++ src/Components/FormaContacto/index.js
@@ -5,8 +5,48 @@ import './FormaContacto.css';
 function FormaContacto() {
 
     return (
-        <div className="home">
-            <h2>FormaContacto</h2>
+        <div className="Header">
+            <div id="form">
+                <h1>Contacto</h1>
+                {/* Formulario */}
+                
+                    <div className="form-group">
+                        <div id="top-form">
+                            <div id="Nombre" className="form-element">
+                                <label htmlFor="nombre">Nombre:</label>
+                                <input type="text" className="form-control" id="nombre" placeholder="Nombre"/>
+                            </div>
+
+                            <div id="Email" className="form-element">
+                                <label htmlFor="exampleInputEmail">Dirección de Correo Electronico: </label>
+                                <input type="email" className="form-control" id="exampleInputEmail"  placeholder="Ingrese Correo" />
+                            </div>
+
+                            <div id="Numero" className="form-element">
+                                <label htmlFor="Numero de Telefono">Numero de Telefono:</label>
+                                <input type="number" className="form-control" id="Numero de Telefono" placeholder="Numero de Telefono"/>
+                            </div>
+
+                        </div>
+
+                        <div id="bottom-form">
+                            <div id="Mensaje" className="form-element">
+                                <label htmlFor="Mensaje">Mensaje:</label>
+                                <textarea className="form-control" id="Mensaje" rows="3"></textarea>
+                            </div>
+
+                            {/* boton de enviar */}
+                            <button type="submit" className="btn btn-primary">Enviar</button>
+                        </div>
+
+                        
+                    </div>
+                     
+                
+
+                
+            </div>
+
         </div>
     )
 }

294f1b8517ee27aa4d42d50789ee4291e149c51b
Carlosrc31 <charlie31rc@gmail.com>
2022-03-11 18:31:20 -0600

    Comienzo

--- src/Components/Busqueda/index.js
+++ src/Components/Busqueda/index.js
@@ -1,11 +1,31 @@
 import React from "react";
-import "./Busqueda.css";
+import './Busqueda.css';
 
 function Busqueda () {
     return (
-        <div className="SearchBox">
-            <h2 className="title">Búsqueda del Panteón</h2>
+        // <div className="SearchBox">
+        //     <h2 className="title">Búsqueda del Panteón</h2>
+        // </div>
+        //Usar un form para que funcione la búsqueda 
+        <div className="table_container">
+            <div className="search_box">
+                <input className="search_input" value={'Buscar'}/>
+                <button className="search_button" type="submit">Lupa</button>
+            </div>
+
+            <div className="filters">
+                <div className="cuartel_filter">
+                    <button className="cuartel_button">Cuartel</button>
+                </div>
+                <div className="clase_filter">
+                    <button className="clase_button">Clase</button>
+                </div>
+                <div className="fosa_filter">
+                    <button className="fosa_button">Fosa</button>
+                </div>
+            </div>
         </div>
+
     )
 }
 

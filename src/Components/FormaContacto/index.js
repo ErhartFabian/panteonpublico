b8893b1d77bbc80ef6c1151692e3a749a0243a56
Erhart Fabian Castillo <A01274089@itesm.mx>
2022-03-12 18:42:50 -0600

    Formulario-Funcionalidad de campos

--- src/Components/FormaContacto/index.js
+++ src/Components/FormaContacto/index.js
@@ -1,51 +1,62 @@
-import React from "react";
+import React,{useState, useRef} from "react";
 import './FormaContacto.css';
-
+import emailjs from '@emailjs/browser';
 
 function FormaContacto() {
+    //imprimir el nomre de la persona en consola
+    const form = useRef();
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+      emailjs.sendForm('service_y1w5vug', 'template_nmhx1uc', form.current, 'KFcgLyPohhWVvEG6y')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+        e.target.reset();
+    };
+    
+    
 
     return (
-        <div className="Header">
-            <div id="form">
+        <div className="area-form">
+            <form id="form" ref={form} >
                 <h1>Contacto</h1>
                 {/* Formulario */}
                 
                     <div className="form-group">
                         <div id="top-form">
                             <div id="Nombre" className="form-element">
-                                <label htmlFor="nombre">Nombre:</label>
-                                <input type="text" className="form-control" id="nombre" placeholder="Nombre"/>
+                                <input maxlength="60" type="text" className="form-control" className="input-group" id="nombre"  placeholder="Nombre" name="user_name"/>
                             </div>
 
                             <div id="Email" className="form-element">
-                                <label htmlFor="exampleInputEmail">Dirección de Correo Electronico: </label>
-                                <input type="email" className="form-control" id="exampleInputEmail"  placeholder="Ingrese Correo" />
+                                <input type="email" className="form-control" className="input-group" id="correo"  placeholder="Ingrese Correo" name="user_email" />
                             </div>
 
                             <div id="Numero" className="form-element">
-                                <label htmlFor="Numero de Telefono">Numero de Telefono:</label>
-                                <input type="number" className="form-control" id="Numero de Telefono" placeholder="Numero de Telefono"/>
+                                <input maxlength="10" type="tel" className="form-control" className="input-group" id="numero-telefono" placeholder="Numero de Telefono" name="numero"/>
                             </div>
 
                         </div>
 
                         <div id="bottom-form">
-                            <div id="Mensaje" className="form-element">
-                                <label htmlFor="Mensaje">Mensaje:</label>
-                                <textarea className="form-control" id="Mensaje" rows="3"></textarea>
-                            </div>
+                            
+                                {/* <label htmlFor="Mensaje">Mensaje:</label> */}
+                                <textarea maxlength="252" id="area-mensaje"  rows="3" placeholder="Mensaje" name="message"></textarea>
+                            
 
                             {/* boton de enviar */}
-                            <button type="submit" className="btn btn-primary">Enviar</button>
+                            <button type="submit" className="enviar-button" value="Send">Enviar</button>
                         </div>
 
                         
                     </div>
-                     
-                
+                 
 
                 
-            </div>
+            </form>
 
         </div>
     )

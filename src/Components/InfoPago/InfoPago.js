cf6a3569c624692eda5c652c093fd9eb161cc7b9
contraaplayer <a01275375@itesm.mx>
2022-04-02 19:49:58 -0600

    Vista ficha de pago

--- src/Components/InfoPago/InfoPago.js
+++ src/Components/InfoPago/InfoPago.js
@@ -1,54 +1,98 @@
+import React, {useState} from 'react';
 import './InfoPago.css'
-import React from 'react';
 import Button from "@material-ui/core/Button";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faReceipt} from '@fortawesome/free-solid-svg-icons';
-import {faFile} from '@fortawesome/free-solid-svg-icons';
-function InfoPago() {
+import Datepicker,{registerLocale} from 'react-datepicker'
+import "react-datepicker/dist/react-datepicker.css";
+import es from 'date-fns/locale/es'
 
+registerLocale("es",es);
+
+function InfoPago() {
+    const [datospago,setDatospago] =useState({})
+
+    const handleChange = e =>{
+        setDatospago({
+            ...datospago,
+            [e.target.name]:e.target.value, 
+        })
+    }
+
+    const[fecha,setFecha] = useState(new Date("2022", "01", "01"));
+
+    const handleSubmit = e =>{
+        let emptyVal=false;
+        e.preventDefault();
+        setDatospago({
+            ...datospago,
+            [e.target.name]:e.target.value, 
+        })
+        if(e.target.value===" "){
+            emptyVal=true;
+        }
+        if(emptyVal === true)
+        {
+            alert("Complete todos los datos");
+        }
+        else{
+            alert("Datos enviados");
+        }
+    }
     return (
         <div className="container">
            
-            <div className='info nombre'>
-                <h1 id="name">Fulanito muerto</h1>
-            
+            <form onSubmit={handleSubmit} className='informacion'>
+                <h1 id="name">Ficha de pago</h1>
+
                 <div className='dato'>
-                    <label id="form1">Tipo de pago: </label> <input className='input' type="text"></input>
+                    <label htmlfor="idpago" id='pago'>Clave de pago </label> 
+                    <input 
+                    className='input'
+                    placeholder='Ingrese clave de pago' 
+                    type="text" 
+                    name="idpago" 
+                    value={datospago.idpago}
+                    onChange={handleChange}
+                    />
                 </div>
 
                 <div className='dato'>
-                    <label id="form2">Cuartel asignado:</label>
-                    <select id="options" name="cuartel">
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                    </select>
+                    <label htmlfor="idfosa" id='fosa'>Clave de fosa </label> 
+                    <input 
+                    className='input' 
+                    type="text"
+                    placeholder='Ingrese clave de fosa'
+                    name="idfosa"
+                    value={datospago.idfosa}
+                    onChange={handleChange}
+                    />
+                </div>
+
+                <div className='dato'>
+                    <label  htmlfor='fecha' id="fechaPago" >Fecha de pago</label>
+                    <div>
+                    <Datepicker
+                    className='datepicker'
+                    selected={fecha}
+                    onChange={(date: fecha) => setFecha(date)}
+                    locale={es}
+                    dateFormat="dd-MMMM-yyyy"/>
+                    </div>
                 </div>
 
                 <div className='generador'>
                     <Button 
+                    type="submit"
                     variant="contained" 
                     color="primary"
-                    size="Large"
+                    size="large"
                     disableElevation>
                        <FontAwesomeIcon className='icono' icon={faReceipt} />
-                        Recibo de pago 
+                        Descargar recibo de pago 
                     </Button>
-
-                    <Button 
-                    id="Acta"
-                    variant="contained"
-                    color="secondary"
-                    size="Large"
-                    disableElevation>
-                     <FontAwesomeIcon className='icono' icon={faFile} />
-                    Acta de defunción</Button>
                 </div>
-            </div>
-            <div>
-            <FontAwesomeIcon icon="faReceip" />
-            </div>
+            </form>
         </div>
     )
 }

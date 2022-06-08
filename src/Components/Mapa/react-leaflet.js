850e11c5ac35834fdb53ad8138e15a09ddd97737
Irving Silva <irving_s123@hotmail.com>
2022-06-08 11:48:52 -0500

    Pase de coordenadas agregado
    Falta paneo en el mapa

--- src/Components/Mapa/react-leaflet.js
+++ src/Components/Mapa/react-leaflet.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, Marker, Popup, TileLayer, useMapEvents } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import './css/react-leaflet.css';
+import { MarkerIcon } from './react-leaflet-icon.js';
 
 class MapView extends Component {
+  handleClick(e){
+    this.setState({ currentLocation: e.latlng });
+  }
+  
+  handleChange(e){
+    e.flyTo(this.props.fosa.coordenadas);
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentLocation: { lat: 38.71, lng: -0.48 },
+      zoom: 12,
+    }
+    this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
   render() {
     return (
-      <MapContainer center={[20.1281, -98.76437]} zoom={17} onClick={this.handleClick} style={{
+      <MapContainer center={[20.1281, -98.76437]} zoom={17} onClick={this.handleClick} onChange={this.handleChange} style={{
         position: "relative",
         minWidth: "700px",
         width: "45%",
@@ -16,6 +34,11 @@ class MapView extends Component {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
         />
+        <Marker position={this.props.fosa.coordenadas} icon={MarkerIcon}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+      </Marker>
       </MapContainer>
     );
   }
